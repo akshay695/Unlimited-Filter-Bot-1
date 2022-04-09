ae1af053f3ee95f6e414e90423ca8f8073d34b7b
akshay695 <102814990+akshay695@users.noreply.github.com>
2022-04-09 15:24:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,10 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/UvinduBro_BOTs"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/UvinduBr")
+                    InlineKeyboardButton("BOT Channel", url="https://telegra.ph/Filters-bot-Update-04-09"),                 
                 ]
             ]
         ),
@@ -262,11 +260,6 @@ async def about(client, message):
         text=Script.ABOUT_MSG,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/UvinduBro/Unlimited-Filter-Bot")
-                ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("CLOSE", callback_data="close_data"),
